97046653e8ea1f0dfa1dece73a024d572d1ba5c6
Luu19MTP <119726478+Luu19MTP@users.noreply.github.com>
2023-09-06 19:19:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Happy birthday Ngoc Anh",
-    introTitle: 'Ngọc Anh',
-    introDesc: `Hôm nay chắc hẳn là một ngày đặc biệt đối với cậu nhỉ. Tớ chúc mừng sinh nhật cậu như là những gì 2 năm qua tớ chúc nha.`,
+    titleWeb: "Happy birthday to you",
+    introTitle: 'Người tuyệt vời nhất',
+    introDesc: `Hôm nay chắc hẳn là một ngày đặc biệt đối với cậu nhỉ. `,
     btnIntro: '^^Happy Brithday^^',
     title: 'Chúc cậu tất cả, Tớ thì cũng vẫn mãi như thế không đổi đâu🥰',
-    desc: 'Chúc cậu và ny của mình sẽ luôn hạnh phúc bên nhau nha',
+    desc: 'Chúc cậu tất cả',
     btnYes: 'Tất nhiên rồi <33',
     btnNo: 'Không người tớ muốn bên cạnh là cậu :3',
     question: 'Có điều gì muốn gửi cho mình hông nè<3',
     btnReply: 'Gửi đến Lưu <3',
     reply: 'Chúc Lưu mạnh khỏe nha',
-    mess: 'Mình biết mà 🥰. Yêu Ngọc Anh nhiều nhiều nha 😘😘',
-    messDesc: 'Cảm ơn Ngọc Anh đã đến bên Lưu vào năm Lưu 17 tuổi nhé',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều nha 😘😘',
+    messDesc: 'Cảm ơncậu',
     btnAccept: 'Bye Lưu ',
     messLink: 'https://youtu.be/7odrfJKxiSs' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
